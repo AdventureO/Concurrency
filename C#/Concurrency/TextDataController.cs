b9d297ed5e5de98840dc9ed31bb3d7b6d02d3835
romko karpo <karpinskyy@ucu.edu.ua>
2017-05-25 15:15:27 +0300

    Add StreamReaderUtility, add ability to skip to line index for StreamReader, divide processed data into chunks thus make return data correct

--- C#/Concurrency/TextDataController.cs
+++ C#/Concurrency/TextDataController.cs
@@ -8,9 +8,9 @@ using System.IO;
 
 namespace Concurrency
 {
-    class TextDataController
+    public class TextDataController
     {
-        public const int AVERAGEWORDSCOUNT = 8000;
+        public const int AVERAGEWORDSCOUNT = 9000;
 
         public Dictionary<string, int> GetWordsCountInParallel(string fileName, int threadsQuantity)
         {
@@ -18,9 +18,11 @@ namespace Concurrency
 
             Thread[] lels = new Thread[threadsQuantity];
 
+            int fileDataChunckIndexStep = File.ReadLines(fileName).Count() / threadsQuantity;
+
             for (int i = 0; i < threadsQuantity; i++)
             {
-                lels[i] = new Thread(new ThreadStart(delegate { this.ReadDataFromFile(fileName, ref wordsCount); }));
+                lels[i] = new Thread(new ThreadStart(delegate { this.ReadDataFromFileInParallel(fileName, ref wordsCount, fileDataChunckIndexStep * i); }));
                 lels[i].Start();
             }
 
@@ -33,7 +35,7 @@ namespace Concurrency
         }
 
         private object _lockObject = new object();
-        private void ReadDataFromFile(string fileName, ref Dictionary<string, int> globalWordsCount, Action callbackAction = null)
+        private void ReadDataFromFileInParallel(string fileName, ref Dictionary<string, int> globalWordsCount, int startingLineIndex, Action callbackAction = null)
         {
             Dictionary<string, int> localWordsCount = new Dictionary<string, int>(globalWordsCount.Count);
             int startingWordCount = 1;
@@ -42,6 +44,8 @@ namespace Concurrency
             {
                 using (StreamReader streamReader = new StreamReader(fileName))
                 {
+                    StreamReaderUtility.SkipToLine(streamReader, startingLineIndex);
+
                     string fileLine = streamReader.ReadLine();
                     string[] fileLineWordsBuffer;
                     while (fileLine != null)
@@ -87,6 +91,49 @@ namespace Concurrency
                         globalWordsCount.Add(word, localWordsCount[word]);
                     }
                 }
+            }
+
+            callbackAction?.Invoke();
+        }
+
+        private void ReadDataFromFile(string fileName, ref Dictionary<string, int> wordsCount, Action callbackAction = null)
+        {
+            int startingWordCount = 1;
+
+            try
+            {
+                using (StreamReader streamReader = new StreamReader(fileName))
+                {
+                    string fileLine = streamReader.ReadLine();
+                    string[] fileLineWordsBuffer;
+                    while (fileLine != null)
+                    {
+                        fileLine = new string(fileLine.Where(c => !char.IsPunctuation(c)).ToArray());
+                        if (fileLine.Trim() != string.Empty)
+                        {
+                            fileLineWordsBuffer = fileLine.Split(' ');
+                            for (int i = 0; i < fileLineWordsBuffer.Length; i++)
+                            {
+                                if (wordsCount.ContainsKey(fileLineWordsBuffer[i]))
+                                {
+                                    wordsCount[fileLineWordsBuffer[i]]++;
+                                }
+                                else
+                                {
+                                    wordsCount.Add(fileLineWordsBuffer[i], startingWordCount);
+                                }
+                            }
+                        }
+
+                        fileLine = streamReader.ReadLine();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read");
+                Console.WriteLine(e.Message);
+                throw;
             }
 
             callbackAction?.Invoke();
