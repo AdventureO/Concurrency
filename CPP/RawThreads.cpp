2575142bc21022f2a0a7f43caeb88d519219e357
Oleg <indrekis@gmail.com>
2017-06-27 17:50:01 +0300

    'Prettified' raw threads code, using framework, developed while refactoring.

--- CPP/RawThreads.cpp
+++ CPP/RawThreads.cpp
@@ -10,166 +10,130 @@
 #include <atomic>
 #include <sstream>
 
+#include "aux_tools.hpp"
+#include "clean_words.hpp"
+
+#ifdef USE_UNORDERED_MAP
+using map_type = std::unordered_map<std::string, unsigned int>;
+#else // Default
+using map_type = std::map<std::string, unsigned int>;
+#endif
+
 using namespace std;
 mutex mtx;
 
 
-//for(x: lm)
-//gm[x.first] += x.sp
-//
-
-vector<string> open_read(string path) {
-    ifstream myfile;
+vector<string> readData(ifstream& file) {
     vector<string> words;
     string word;
-    myfile.open(path);
-    if (!myfile.is_open()) {
-        cerr << "Error" << endl;
-        return words;
-    }
-    string formated_word;
-    while (myfile >> word) {
+    while (file >> word) {
         words.push_back(word);
     }
-    myfile.close();
     return words;
 }
 
 
-int finder(vector<string>& myVector, int start, int end, std::map<string, int>& m)
+void wordCounter(vector<string>& myVector, size_t start, size_t end, map_type& m)
 {
-    map<string, int> localMp;
+    map_type localMp;
 
-    for (int i = start; i < end; i++) {
+    for (size_t i = start; i < end; i++) {
         {
-            myVector[i].erase(remove_if(myVector[i].begin(), myVector[i].end(), [](char x) {return !isalpha(x);} ),myVector[i].end() );
-            transform(myVector[i].begin(), myVector[i].end(), myVector[i].begin(), ::tolower);
+            cleanWord(myVector[i]);
             ++localMp[myVector[i]];
         }
     }
     lock_guard<mutex> lg(mtx);
 
-    for(map<string, int> :: iterator i = localMp.begin(); i != localMp.end(); i++){
-        m[i->first] += i-> second;
+    for(auto& item: localMp){
+        m[item.first] += item.second;
 
-    }
-
-    return 0;
-
-}
-
-void write_to_file(const map<string, int> &m, string path) {
-    ofstream myfile;
-    myfile.open(path);
-    for (auto elem : m) {
-        myfile << elem.first << "    " << elem.second << "\n";
-    }
-    myfile.close();
-}
-
-void printMap(const map<string, int> &m) {
-    for (auto elem : m) {
-        cout << elem.first << " : " << elem.second << "\n";
     }
 }
 
-vector<int> SplitVector(const vector<string>& vec, int n) {
 
-    vector<int> outVec;
-    int length = int(vec.size())/ n;
-    int count = 0;
-    int sum = 0;
+vector<size_t> SplitVector(const vector<string>& vec, int n) {
 
+    vector<size_t> outVec;
+    auto part_length = vec.size() / n;
+    size_t sum = 0;
     outVec.push_back(0);
-    while(count != n - 1){
-        sum += length;
+
+    for(int i = 0; i<n-1;++i){
+        sum += part_length;
         outVec.push_back(sum);
-        //cout<<outVec[count]<<endl;
-        count++;
+        // cout<<outVec[i]<<endl;
     }
-    outVec.push_back(int(vec.size()));
+    outVec.push_back(vec.size());
     return outVec;
 }
 
 
-inline std::chrono::high_resolution_clock::time_point get_current_time_fenced()
-{
-    std::atomic_thread_fence(memory_order_seq_cst);
-    auto res_time = std::chrono::high_resolution_clock::now();
-    std::atomic_thread_fence(memory_order_seq_cst);
-    return res_time;
-}
-
-template<class D>
-inline long long to_us(const D& d)
-{
-    return std::chrono::duration_cast<chrono::microseconds>(d).count();
-}
-
 int main(int argc, char *argv[]) {
-    istringstream ss1(argv[1]);
-    int numT;
-    ss1 >> numT;
-    string input_data[2], infile, out_by;
-    ifstream myFile;
-    myFile.open("data_input.txt");
+    auto config = read_config("data_input_conc.txt");
 
-    for(int i = 0; i < 2; i++)
-        myFile >> input_data[i];
-    myFile.close();
+    string infile    = config["infile"];
+    string out_by_a  = config["out_by_a"];
+    string out_by_n  = config["out_by_n"];
+    int    threads_n = str_to_val<unsigned>(config["threads"]);
 
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < input_data[i].length(); j++) {
-            if (input_data[i][j] == '=')
-                input_data[i][j] = ' ';
-        }
-        stringstream ss(input_data[i]);
-        string temp;
-        int k = 0;
-        while (ss >> temp) {
-            if (k != 0) {
-                stringstream lineStream(temp);
-                string s;
-                getline(lineStream,s,',');
-                s.erase(remove( s.begin(), s.end(), '\"' ), s.end());
-                input_data[i] = s;
-            }
-            k++;
-        }
-    }
-
-    infile = input_data[0];
-    out_by = input_data[1];
-
-
-    vector<string> words;
-    words = open_read(infile);
+    string etalon_a_file  = config["etalon_a_file"];
 
     auto start = get_current_time_fenced();
-    vector<pair<string, int>> VectorOfPair;
-    map<string, int> m;
-    thread threads[numT];
-    vector<int> list_of_words_amount = SplitVector(words, numT);
-    auto startPar = get_current_time_fenced();
-    for (int a = 0; a < list_of_words_amount.size()-1; ++a) {
 
-        threads[a] = thread(finder, ref(words), list_of_words_amount[a],
-                            list_of_words_amount[a + 1], ref(m));
+    ifstream data_file(infile);
+    if (!data_file.is_open()) {
+        cerr << "Error reading from file: " << infile << endl;
+        return 1;
+    }
+    vector<string> words{readData(data_file)};
+    data_file.close();
+
+    auto readed = get_current_time_fenced();
+
+    map_type wordsMap;
+    vector<thread> threads;
+    vector<size_t> work_parts = SplitVector(words, threads_n);
+    auto startPar = get_current_time_fenced();
+    for (int a = 0; a < work_parts.size()-1; ++a) {
+
+        threads.emplace_back( wordCounter, ref(words), work_parts[a],
+                              work_parts[a + 1], ref(wordsMap) );
     }
 
     for (auto& th : threads) th.join();
 
-    auto finishPar = get_current_time_fenced();
-    auto counting = finishPar - startPar;
-    write_to_file(m , "result_word.txt");
-    auto finish = get_current_time_fenced();
-    auto total = finish - start;
-    ofstream myfile;
-    myfile.open (out_by);
-    myfile << to_us(counting)<<'\n';
-    myfile << to_us(total)<<'\n';
-    myfile.close();
-    //printMap(m);
+    auto finished = get_current_time_fenced();
+
+    ofstream file(out_by_a);
+    if (!file) {
+        cerr << "Could not open file."<< endl;
+        return 1;
+    }
+    write_sorted_by_key(file, wordsMap);
+    file.close();
+
+    //Write in file words by alphabet
+    ofstream file2(out_by_n);
+    if (!file2) {
+        cerr << "Could not open file " << out_by_n << endl;
+        return 1;
+    }
+    write_sorted_by_value(file2, wordsMap);
+    file2.close();
+
+    auto counting = finished - readed;
+    auto total = finished - start;
+
+    cout << "Total time    : " << to_us(total) << endl;
+    cout << "Analisys time : " << to_us(counting) << endl;
+
+    bool are_correct = true;
+    if( !etalon_a_file.empty() )
+    {
+        are_correct = compareFiles(out_by_a, etalon_a_file);
+    }
+    return !are_correct;
 
     return 0;
 
