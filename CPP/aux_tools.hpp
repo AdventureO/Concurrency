62ed6b4be1bc5fc66ee05966525dc1bf5a953b2e
Oleg <indrekis@gmail.com>
2017-06-28 05:12:40 +0300

    Suppress (most of) stupid VC warning 4503

--- CPP/aux_tools.hpp
+++ CPP/aux_tools.hpp
@@ -12,7 +12,6 @@
 #include <map>
 #include <unordered_map>
 
-
 inline std::chrono::high_resolution_clock::time_point get_current_time_fenced() {
     std::atomic_thread_fence(std::memory_order_seq_cst);
     auto res_time = std::chrono::high_resolution_clock::now();
