3332c6690d5428e1d63e0fb40509d853a8700425
Oleg <indrekis@gmail.com>
2017-07-03 22:53:33 +0300

    Forgotten PVS studio comment

--- cpp_qt/q_seq_etalon.cpp
+++ cpp_qt/q_seq_etalon.cpp
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 #include <iostream>
 
 #include <QtDebug>
@@ -12,7 +15,7 @@
 using map_type = QMap<QString, unsigned int>;
 using string_list_type = QStringList; //  QVector<QString>
 
-class CCException: QException
+class CCException: public QException
 {
     QString m_message;
 public:
