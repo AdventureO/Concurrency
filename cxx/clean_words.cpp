077262628a660f57a2be46f222f45fcb59bde98b
Oleg <indrekis@gmail.com>
2017-07-03 22:01:40 +0300

    Use pure ASCII symbols working with text -- full locales and UTF-8 symbols is not needed and too hard to implement in portable and consistent way across all platforms.

--- cxx/clean_words.cpp
+++ cxx/clean_words.cpp
@@ -10,11 +10,11 @@ void cleanWord(std::string &word)
 {
 #ifdef _MSC_VER
 	// isXXX function have assertions in MSVC...
-	word.erase(remove_if(word.begin(), word.end(), [](unsigned char a) { return ispunct(a); }), word.end());
+    word.erase(remove_if(word.begin(), word.end(), [](unsigned char a) { return !isalnum(a); }), word.end());
 	transform(word.begin(), word.end(), word.begin(), [](unsigned char a) { return tolower(a); });
 
 #else
-    word.erase( remove_if(word.begin(), word.end(), ::ispunct), word.end() );
+    word.erase( remove_if(word.begin(), word.end(), [](char x){ return !isalnum(x);}), word.end() );
     transform(word.begin(), word.end(), word.begin(), ::tolower);
 #endif 
 }
