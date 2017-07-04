2b34750a7ae831865e9f3fd1d2b04e54b49ade8e
Oleg <indrekis@gmail.com>
2017-07-04 03:33:05 +0300

    Added mapReduce

--- cpp_qt/QtConcurrentP.cpp
+++ cpp_qt/QtConcurrentP.cpp
@@ -1,213 +1,131 @@
 #include <iostream>
+#include <QtDebug>
 #include <QTextStream>
 #include <QString>
 #include <QMap>
-#include <cmath>
-#include <iterator>
-#include <QThread>
-#include <QMutex>
-#include <QWaitCondition>
-#include <QTime>
-#include "timing_v1.hpp"
-#include <QtConcurrent/QtConcurrentRun>
+#include <QtConcurrent>
 #include <QFile>
 #include <QCoreApplication>
-#include <algorithm>
 
+#include "q_clean_words.hpp"
 
-//COMMAND LINE ARGUMENTS:  5 /home/yaryna/Desktop/ sec.txt RESULT_FOR_TEST.txt 9 "LAST"
+//! Немає сенсу переписувати допоміжні інструменти на Qt
+#include "../cxx/aux_tools.hpp"
 
+using map_type = QMap<QString, unsigned int>;
+using string_list_type = QStringList;
+//using string_list_type = QVector<QString>;
 
-using namespace std;
-
-using words_counter_t = QMap<QString, int>;
-words_counter_t words;
-QMutex mutex;
-QWaitCondition bufferNotEmpty;
-
-QStringList reading1(const QString& filename) {
-    QStringList lst;
+string_list_type qtReadData(const QString& filename) {
+    string_list_type lst;
     QFile inputFile(filename);
-    QString l;
-    if (inputFile.open(QIODevice::ReadOnly))
-    {
-         QTextStream in(&inputFile);
-    l = in.readAll();
-   for (QChar el : l) {
-       if ((el == (QChar)'=')|| (el == (QChar)'\xa')) {
-           l.replace('=', ' ');
-           l.replace('\xa',  ' ');
-           l.replace('"',  ' ');
-}
-}
-   lst << l.simplified().split(' ', QString::SkipEmptyParts);
-
-   inputFile.close();
-
-}
-return lst;
-}
-
-QStringList reading(const QString& filename) {
-    QStringList lst;
-    QFile inputFile(filename);
-    QString l;
+    QString word;
     if (inputFile.open(QIODevice::ReadOnly)) {
-
-            QTextStream in(&inputFile);
-            while (!in.atEnd()) {
-            in >> l;
-    lst << l;
-}
-
-
-       inputFile.close();
-
+        QTextStream in(&inputFile);
+        in.setCodec("UTF-8");   // Ми враховуємо лише ASCII-символи, але
+                                // це потрібно для коректного (ну, чи просто --
+                                // такого ж, як в "голому" С++) розбиття на слова
+        while (!in.atEnd()) {
+            in >> word;
+            lst.append(word);
+        }
     }
     return lst;
 }
 
-QList<int> lst_division(QStringList& data_lst, int threads) {
-    QList<int> general;
-    int pointer = 0;
-  //  if da
-    int division = std::ceil((float)data_lst.size()/threads);
-        while (pointer+division < data_lst.size()) {
+using work_interval = QPair<string_list_type::iterator, string_list_type::iterator>;
 
-            general.append(pointer);
-            general.append(pointer+division);
-
-            pointer += division + 1;
-        }
-
-        if (pointer < data_lst.size()) {
-            general.append(pointer);
-            general.append(data_lst.size()-1);
-            pointer = data_lst.size();
-        }
+QVector<work_interval> splitWork(string_list_type& data_lst, size_t threads) {
+    QVector<work_interval> general;
+    auto part_length = data_lst.size() / threads;
+    auto start = data_lst.begin();
+    auto finish = start;
+    std::advance(finish, part_length);
+    general.append( qMakePair(start, finish) );
+    for(size_t i = 0; i<threads-1;++i){
+        start = finish;
+        std::advance(finish, part_length);
+        general.append(qMakePair(start, finish));
+    }
+    general.append(qMakePair(finish, data_lst.end()));
     return general;
 }
 
-words_counter_t mapper(int num_start, int num_fin, QStringList& data_lst) {
-    words_counter_t words_local;
-    for (int a=num_start; a<=num_fin; a++) {
-           data_lst[a] =  data_lst[a].remove(remove_if(data_lst[a].begin(), data_lst[a].end(), [](QChar x) {return !x.isLetter() && !x.isSpace();} )
-                                - data_lst[a].begin(), data_lst[a].size() ).toUpper();
-
-               ++words_local[data_lst[a]];
-           }
-
-    return words_local;
+map_type mapper(const work_interval& work) {
+    map_type local_dictionary;
+    for (auto a=work.first; a<work.second; a++) {
+        qtCleanWord(*a);
+        ++local_dictionary[*a];
+    }
+    return local_dictionary;
 }
 
-void reducer(words_counter_t &words, const words_counter_t& local_dictionary) {
+void reducer(map_type &words, const map_type& local_dictionary) {
     for(auto itr=local_dictionary.cbegin(); itr!=local_dictionary.cend(); ++itr)
-        {
-            words[itr.key()]+=itr.value();
-        }
+    {
+        words[itr.key()]+=itr.value();
+    }
 }
 
 int main(int argc, char *argv[])
 {
-   // ----------------------------------------------
-   // reading from command promt
+    setlocale(LC_ALL,"C");
 
     QCoreApplication app(argc, argv);
 
-    QString in_filename, out_filename;
-    int num_threads;
+    auto config = read_config("data_input_conc.txt");
 
-    QString myargfile("/home/yaryna/AKS_main/data_input.txt");
-    QFile myFile();
+    QString infile      = QString::fromStdString(config["infile"]);
+    QString out_by_a    = QString::fromStdString(config["out_by_a"]);
+    QString out_by_n    = QString::fromStdString(config["out_by_n"]);
+    size_t threads_n    = str_to_val<size_t>(config["threads"]);
 
-    QStringList lst_arg = reading1(myargfile);
-      //  sscanf(argv[1], "%d", &num_threads);
+    QString etalon_a_file  = QString::fromStdString(config["etalon_a_file"]);
 
-    //    QString base_path(argv[2]);
+    //=============================================================
+    auto reading_start_time = get_current_time_fenced();
 
-        in_filename = lst_arg[1];
-        out_filename = lst_arg[3];
-        num_threads = stoi(argv[1]);
-
-
-    // ----------------------------------------------
-
-    auto creating_threads_start_time = get_current_time_fenced();
-
-    QStringList words_lst = reading(in_filename); //skip punctuation
-
-
-    QMap<QString, int> result_futures;
+    string_list_type words_lst = qtReadData(infile);
     if (words_lst.isEmpty()) {
-       cerr << "No data in the file or mistake in configuration"<< endl;
-       return -1;
+        std::cerr << "No data in the file or mistake in configuration" << std::endl;
+        return -1;
     }
-    if (words_lst.size()<num_threads) num_threads=words_lst.size()-1; //!!!
-    QList<int> num_lst = lst_division(words_lst, num_threads);
+    Q_ASSERT_X( words_lst.size() >= threads_n, "main", "Too small file -- should contain more words than threads started.");
 
+    auto work_parts = splitWork(words_lst, threads_n);
 
-    QList<QFuture<words_counter_t>> future_list;
-
-    int num_pointer = 0;
     auto indexing_start_time = get_current_time_fenced();
 
-    for (int el=0; el<num_threads; el++) {
-       // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-       if (num_pointer+1 < num_lst.size()) {
-       future_list << QtConcurrent::run(mapper, num_lst[num_pointer], num_lst[num_pointer+1], words_lst);
-               num_pointer += 2;
-       }
+    auto resultFuture = QtConcurrent::mappedReduced(work_parts, mapper, reducer);
+
+    map_type wordsMap = resultFuture.result();
+
+    auto indexing_done_time = get_current_time_fenced();
+    //=============================================================
+    auto indexing_time = to_us(indexing_done_time - indexing_start_time);
+    //auto reading_time = to_us(indexing_start_time - reading_start_time);
+    auto total_time = to_us(indexing_done_time - reading_start_time);
+
+    std::cout << "Total time    : " << total_time << std::endl;
+    std::cout << "Analisys time : " << indexing_time << std::endl;
+
+    //=============================================================
+    //! Чисто з ліні -- щоб не переносити функції збереження під Qt
+    std::map<std::string, unsigned int> cpp_map;
+    for(auto iter = wordsMap.begin(); iter != wordsMap.end(); ++iter)
+    {
+        cpp_map[iter.key().toStdString()] = iter.value();
     }
 
+    write_sorted_by_key(out_by_a.toStdString(), cpp_map);
+    write_sorted_by_value(out_by_n.toStdString(), cpp_map);
 
-
-    for (size_t el=0; el<future_list.size(); el++) {
-        reducer(words, future_list[el]);
-        //words << future_list[el].result();
+    bool are_correct = true;
+    if( !etalon_a_file.isEmpty() )
+    {
+        are_correct = compareFiles(out_by_a.toStdString(), etalon_a_file.toStdString());
     }
-
-
-
-   //---------------------------------------------------------------
-   // Це все ще не дуже хороший спосіб виміру! Потім мусимо переключитися
-   // на Performance counters, але це буде потім, а так -- краще, ніж QTimer.
-   // Performance counters -- див. PAPI тут:
-   // Архітектура комп'ютерних систем (CS.02.17) --> Практична 2. Розпаралелення задач із явним використанням потоків ОС --> Вимірювання часу
-   // (Пряме посилання не даю, з міркувань безпеки).
-
-   auto indexing_done_time = get_current_time_fenced();
-
-   int total_words = 0;
-   for(auto it = words.begin(); it != words.end(); ++it) {
-       total_words += it.value();
-   }
-
-   auto threading_time = to_us(indexing_done_time - indexing_start_time);
-   auto reading_time = to_us(indexing_start_time - creating_threads_start_time);
-   auto total_time = to_us(indexing_done_time - creating_threads_start_time);
-
-
-//   //---------------------------------------------------------------
-   QFile output_file(out_filename);
-   if (!output_file.open(QIODevice::WriteOnly)) {
-       cerr << "Could not write file with results." << endl;
-       return -1;
-   }
-
-   QTextStream output_stream(&output_file);
-
-
-
-   output_stream << reading_time << endl;
-   output_stream << threading_time << endl;
-   output_stream << total_time << endl;
-
-
-//   for (auto it = words.begin(); it != words.end(); ++it) {
-//           // Format output here.
-//           output_stream << QString("%1 : %2 \n").arg(it.key(), 10).arg(it.value(), 10);
-//   }
-   output_file.close();
+    return !are_correct;
 }
 
 
