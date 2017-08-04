86a45772a9421abda11bde3cec1baf39354c9ec8
Oleg <indrekis@gmail.com>
2017-08-04 11:49:58 +0300

    Modified Python RawThreads to use common configuration file

--- Python/raw_threads.py
+++ Python/raw_threads.py
@@ -3,9 +3,20 @@ import multiprocessing
 from time import time
 from string import punctuation
 
+def read_config(filename):
+    res = {}
+    with open(filename, 'r', encoding='utf-8') as conf_file:
+        for line in conf_file:
+            line = line.split('#',1)[0].strip() # Remove comments
+            if not line:
+                continue
+            name,val = (x.strip() for x in line.split('=', 1))
+            res[name] = val
+        return res
+
 def read_file(file_name):
     words_list = []
-    for line in open(file_name, 'r'):
+    for line in open(file_name, 'r', encoding='utf-8'):
         for word in line.translate(line.maketrans("", "", punctuation)).lower().split():
             words_list.append(word)
 
@@ -21,32 +32,39 @@ def write_file(word_counter, file_name):
         for (word, occurance) in word_counter.items():
             file.write('{:15}{:3}\n'.format(word, occurance))
 
-"""
-Зміна між потоками та процесами
-"""
-while True:
-    version = str(input("For threading enter - t, for multiprocessing enter - m: "))
-    number_of_threads = int(input("Enter a number of threads/processes: "))
+#! Винесено за межі "if __name__ == '__main__':"
+# Щоб могти успадкувати WordsCount у кожному із субпроцесів
+# Виглядає неохайно, зате просто.
+config = read_config('data_input_conc.txt')
 
-    if version == "t" and number_of_threads > 0:
-        parent_class = threading.Thread
-        lock_type = threading.Lock()
-        break
+infile    = config["infile"]
+out_by_a  = config["out_by_a"]
+out_by_n  = config["out_by_n"]
+threads_n = int(config["threads"])
 
-    elif version == "m" and number_of_threads > 0:
-        parent_class = multiprocessing.Process
-        lock_type = multiprocessing.Lock()
-        break
+etalon_a_file = config["etalon_a_file"]
+# measurement_flags = int(config["measurement_flags"])
+
+python_flags=config["python_flags"]
+if 't' in python_flags:
+    parent_class = threading.Thread
+    lock_type = threading.Lock()
+elif 'p' in python_flags:
+    parent_class = multiprocessing.Process
+    lock_type = multiprocessing.Lock()
+else:
+    raise Exception('Neither threads no processes concurrency selected')
+
+number_of_threads = threads_n
 
 
-#class WordsCount(threading.Thread):
-#class WordsCount(multiprocessing.Process):
 class WordsCount(parent_class):
 
     word_counter = {}
     lock = lock_type
-    #lock = multiprocessing.Lock()
-    #lock = threading.Lock()
+
+    # lock = multiprocessing.Lock()
+    # lock = threading.Lock()
 
     def __init__(self, words_list):
         super().__init__()
@@ -69,32 +87,32 @@ class WordsCount(parent_class):
                 else:
                     WordsCount.word_counter[i] = local_dict[i]
 
+if __name__ == '__main__':
 
-input_list = read_file('text1.txt')
-threads = []
+    input_list = read_file('text1.txt')
+    threads = []
 
-avg = len(input_list) / number_of_threads
-last = 0
+    avg = len(input_list) / number_of_threads
+    last = 0
 
-while last < len(input_list):
-    threads.append(WordsCount(input_list[int(last):int(last + avg)]))
-    last += avg
+    while last < len(input_list):
+        threads.append(WordsCount(input_list[int(last):int(last + avg)]))
+        last += avg
 
-start_time = time()
+    start_time = time()
 
-for thread in threads:
-    thread.start()
+    for thread in threads:
+        thread.start()
 
-for thread in threads:
-    thread.join()
+    for thread in threads:
+        thread.join()
 
-work_time = time() - start_time
-if version == "t":
-    print('Got {} threads in {} seconds'.format(len(threads), work_time))
+    work_time = time() - start_time
+    if 't' in python_flags:
+        print('Got {} threads in {} seconds'.format(len(threads), work_time))
+    else:
+        print('Got {} processes in {} seconds'.format(len(threads), work_time))
 
-else:
-    print('Got {} processes in {} seconds'.format(len(threads), work_time))
-
-print(WordsCount.word_counter)
-write_file(WordsCount.word_counter, 'result.txt')
+    print(WordsCount.word_counter)
+    write_file(WordsCount.word_counter, 'result.txt')
 
